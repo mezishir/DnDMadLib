039e75ef1f3c511cf02f0657dff6926eaf7277b4
mezishir <koden.sorceress@gmail.com>
2024-07-24 13:22:43 -0500

    updates

--- main.js
+++ main.js
@@ -1,55 +1,81 @@
-//constants to save inputs
-const heroname = document.getElementById("heroname");
-const vname = document.getElementById("vname");
-const townname = document.getElementById("townname");
-const item1 = document.getElementById("item1");
-const item2 = document.getElementById("item2");
-const verb1 = document.getElementById("verb1");
-const verb2 = document.getElementById("verb2");
-const verb3 = document.getElementById("verb3");
-const verb4 = document.getElementById("verb4");
-const adj1 = document.getElementById("adj1");
-const adj2 = document.getElementById("adj2");
-const adj3 = document.getElementById("adj3");
-const adj4 = document.getElementById("adj4");
-const adj5 = document.getElementById("adj5");
-const adj6 = document.getElementById("adj6");
-const adj7 = document.getElementById("adj7");
 
 //Changing variables
 let restart = document.querySelector(".restart");
 let generate = document.querySelector(".bbtn");
 let afterstory = document.querySelector(".astory");
 let beforestory = document.querySelector(".bstory");
-
+//let cleared = document.querySelectorAll(".inputs");
 
 //Stories to randomly choose
-const story1 = "Our hero, :heroname, was :adj1 towards :townname when he suddenly encountered a great beast!\nIt was the legendary :vname!\nLocal legends have told about this :adj2 creature.\nIt :adj3 its :item1 and :verb1 our Hero.\nOur Hero was :adj4.\:heroname :verb2 his :item2 and :verb3 at the creature.\nThe :adj5 beast was adj6.\nThe Hero :verb4 at his :townname to :adj7 applause.";
-const story2 = "Our hero, :heroname, was :adj1 towards :townname when he suddenly encountered a great :dndclass!\nIt was the legendary :vname!\nLocal Legends have told about this :adj2 villain.\nIt revealed its :adj3 :item1 and :verb1 our Hero.\nOur Hero was :adj4.\n:heroname :verb2 his :item2 and :verb3 the villain.\nThe :adj5 villain was :adj6.\nThe hero :verb4 at :townname to :adj7 applause.";
-const story3 = "Our hero, :heroname, was :adj1 towards :townname when he came across a damsel.\nShe was a beautiful woman named :vname.\n:heroname had heard of this damsel from local lore talking of her :adj2 beauty.\nHero :adj3 his best to :verb1 the lady with his skills in :item1.\nShe was :adj4.\n:vname :verb2 Hero away with a :verb3 of her :item2.\n:heroname was :adj5 to leave the damsel and her :adj6 smile.\nThe hero arrived at :townname :adj7.";
+var story1 = "Our hero, :heroname, was :adj1 towards :townname when he suddenly encountered a great beast!\nIt was the legendary :vname!\nLocal legends have told about this :adj2 creature.\nIt :adj3 its :item1 and :verb1 our Hero.\nOur Hero was :adj4.\:heroname :verb2 his :item2 and :verb3 at the creature.\nThe :adj5 beast was adj6.\nThe Hero :verb4 at his :townname to :adj7 applause.";
+var story2 = "Our hero, :heroname, was :adj1 towards :townname when he suddenly encountered a great :dndclass!\nIt was the legendary :vname!\nLocal Legends have told about this :adj2 villain.\nIt revealed its :adj3 :item1 and :verb1 our Hero.\nOur Hero was :adj4.\n:heroname :verb2 his :item2 and :verb3 the villain.\nThe :adj5 villain was :adj6.\nThe hero :verb4 at :townname to :adj7 applause.";
+var story3 = "Our hero, :heroname, was :adj1 towards :townname when he came across a damsel.\nShe was a beautiful woman named :vname.\n:heroname had heard of this damsel from local lore talking of her :adj2 beauty.\nHero :adj3 his best to :verb1 the lady with his skills in :item1.\nShe was :adj4.\n:vname :verb2 Hero away with a :verb3 of her :item2.\n:heroname was :adj5 to leave the damsel and her :adj6 smile.\nThe hero arrived at :townname :adj7.";
+
+//story1 = story1.replaceAll(":heroname", heroname1);
 
 const stories = [story1, story2, story3];
 const dndclass = ["warlock", "wizard", "sorcerer", "paladin", "druid", "cleric", "bard", "barbarian", "artificer", "rogue", "ranger", "fighter", "monk"]
-const newStory = randomValueFromArray("stories");
-const classes = randomValueFromArray("dndclass");
-let cleared = document.querySelectorAll(".inputs");
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+var newStory = randomValueFromArray(stories);
+var classes = randomValueFromArray(dndclass);
+
+function randomValueFromArray(stories){
+  var numberofstories = stories.length;
+  var randStory = Math.floor(Math.random()*numberofstories);
+  var selectStory = stories[randStory];
+  return selectStory;
 }
-
+function randomValueFromArray(dndclasses){
+  var numberofclasses = dndclasses.length;
+  var randClass = Math.floor(Math.random()*numberofclasses);
+  var selectClass = dndclasses[randClass];
+  return selectClass;
+}
 
 //generate story
 generate.addEventListener("click", updateStory);
 
 function updateStory() {
-    document.querySelector("updated").innerText = newStory;
-    
-    // genStory = newStory.replaceAll(":townname", townname.value);
-    // genStory = newStory.replaceAll(":heroname", heroname.value);
-    // genStory = newStory.replace(":dndclass", classes);
-    
+  //variables saved from inputs
+  let heroname1 = document.getElementById("heroname").value;
+  let townname1 = document.getElementById("townname").value;
+  let vname = document.getElementById("vname").value;
+  let item1 = document.getElementById("item1").value;
+  let item2 = document.getElementById("item2").value;
+  let verb1 = document.getElementById("verb1").value;
+  let verb2 = document.getElementById("verb2").value;
+  let verb3 = document.getElementById("verb3").value;
+  let verb4 = document.getElementById("verb4").value;
+  let adj1 = document.getElementById("adj1").value;
+  let adj2 = document.getElementById("adj2").value;
+  let adj3 = document.getElementById("adj3").value;
+  let adj4 = document.getElementById("adj4").value;
+  let adj5 = document.getElementById("adj5").value;
+  let adj6 = document.getElementById("adj6").value;
+  let adj7 = document.getElementById("adj7").value;
+   
+   
+   //replacement in string
+    newStory = newStory.replaceAll(":heroname", heroname1);
+    newStory = newStory.replaceAll(":townname", townname1);
+    newStory = newStory.replace(":dndclass", classes);
+    newStory = newStory.replaceAll(":vname", vname);
+    newStory = newStory.replace(":item1", item1);
+    newStory = newStory.replace(":item2", item2);
+    newStory = newStory.replace(":verb1", verb1);
+    newStory = newStory.replace(":verb2", verb2);
+    newStory = newStory.replace(":verb3", verb3);
+    newStory = newStory.replace(":verb4", verb4);
+    newStory = newStory.replace(":adj1", adj1);
+    newStory = newStory.replace(":adj2", adj2);
+    newStory = newStory.replace(":adj3", adj3);
+    newStory = newStory.replace(":adj4", adj4);
+    newStory = newStory.replace(":adj5", adj5);
+    newStory = newStory.replace(":adj6", adj6);
+    newStory = newStory.replace(":adj7", adj7);
+   
+    //create story. hide unneccessary divs
+    document.querySelector("#updated").textContent = newStory;
     generate.style.visibility = "hidden";
     beforestory.style.visibility = "hidden";
     restart.style.visibility = "visible";
@@ -57,15 +83,20 @@ function updateStory() {
 }
 
 //reset story
-restart.addEventListener("click", changeBack);
+  restart.addEventListener("click", changeBack);
 
-function changeBack() {
-    beforestory.style.visibility = "visible";
-    restart.style.visibility = "hidden";
-    afterstory.style.visibility = "hidden";
-    generate.style.visibility = "visible";
-    cleared.value = "";
-}
+  function changeBack() {
+    //story is not regenerating. Remaining code not needed.
+    //Story regens on page reload.
+    //Also clears values of inputs.
+    //Leaving this here in case needed in future.
+      // beforestory.style.visibility = "visible";
+      // restart.style.visibility = "hidden";
+      // afterstory.style.visibility = "hidden";
+      // generate.style.visibility = "visible";
+      // cleared.value = "";
+      window.location.reload();
+  }
 
 
 
